348730d72e08e4d26accb084d2a01c0a958b4b08
saikrishnayendava-debug <saikrishnayendava@gmail.com>
2026-02-18 15:35:34 +0530

    Create AdUnit.jsx

---
+++ src/Components/AdUnit.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+export default function AdUnit() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://pl28735180.effectivegatecpm.com/9655042b83c3e89d71ab71635098f4f4/invoke.js';
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div id="container-9655042b83c3e89d71ab71635098f4f4"></div>;
+}
