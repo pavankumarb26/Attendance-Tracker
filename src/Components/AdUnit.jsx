038eb935f36c11482a9f40df3fc357c4188b8cdb
saikrishnayendava-debug <saikrishnayendava@gmail.com>
2026-02-18 15:43:25 +0530

    Update AdUnit.jsx

--- src/Components/AdUnit.jsx
+++ src/Components/AdUnit.jsx
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function AdUnit() {
+  const containerRef = useRef(null);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://pl28735180.effectivegatecpm.com/9655042b83c3e89d71ab71635098f4f4/invoke.js';
     script.async = true;
     script.setAttribute('data-cfasync', 'false');
-    document.body.appendChild(script);
+    
+    // Append script INSIDE the container div
+    if (containerRef.current) {
+      containerRef.current.appendChild(script);
+    }
 
     return () => {
-      document.body.removeChild(script);
+      if (containerRef.current) {
+        containerRef.current.innerHTML = '';
+      }
     };
   }, []);
 
-  return <div id="container-9655042b83c3e89d71ab71635098f4f4"></div>;
+  return (
+    <div 
+      ref={containerRef}
+      id="container-9655042b83c3e89d71ab71635098f4f4"
+      className="w-full my-4"
+    >
+    </div>
+  );
 }
